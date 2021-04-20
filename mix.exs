8d6a9989d4a4ad7b60dfac52fb8c2d9ffe41260b
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-20 03:43:55 -0300

    Added credo deps

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Inmana.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -57,7 +58,7 @@ defmodule Inmana.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test --color"]
     ]
   end
 end
