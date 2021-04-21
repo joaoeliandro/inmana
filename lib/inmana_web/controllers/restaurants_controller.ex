72868967df6e266167803045bd64caf11efd2a09
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-21 01:34:00 -0300

    Added restaurant controller

---
+++ lib/inmana_web/controllers/restaurants_controller.ex
@@ -0,0 +1,17 @@
+defmodule InmanaWeb.RestaurantsController do
+  use InmanaWeb, :controller
+
+  alias Inmana.Restaurant
+
+  alias InmanaWeb.FallbackController
+
+  action_fallback FallbackController
+
+  def create(conn, params) do
+    with {:ok, %Restaurant{} = restaurant} <- Inmana.create_restaurant(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", restaurant: restaurant)
+    end
+  end
+end
