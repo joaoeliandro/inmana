67d06b0821367728bfa5ee7e499433c568822bbb
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-21 22:35:56 -0300

    Added facede create and get supply

--- lib/inmana.ex
+++ lib/inmana.ex
@@ -1,5 +1,10 @@
 defmodule Inmana do
-  alias Inmana.Restaurants.Create
+  alias Inmana.Restaurants.Create, as: RestaurantCreate
+  alias Inmana.Supplies.Create, as: SupplyCreate
+  alias Inmana.Supplies.Get, as: SupplyGet
 
-  defdelegate create_restaurant(params), to: Create, as: :call
+  defdelegate create_restaurant(params), to: RestaurantCreate, as: :call
+
+  defdelegate create_supply(params), to: SupplyCreate, as: :call
+  defdelegate get_supply(uuid), to: SupplyGet, as: :call
 end
