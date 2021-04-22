45ab7a3b743f7d968fab9b4f60eca9171828b7ff
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-21 22:34:29 -0300

    Added render default error

--- lib/inmana_web/views/error_view.ex
+++ lib/inmana_web/views/error_view.ex
@@ -13,15 +13,19 @@ defmodule InmanaWeb.ErrorView do
   # the template name. For example, "404.json" becomes
   # "Not Found".
   def template_not_found(template, _assigns) do
-    %{errors:
-     %{
-      detail: Phoenix.Controller.status_message_from_template(template)
+    %{
+      errors: %{
+        detail: Phoenix.Controller.status_message_from_template(template)
       }
     }
   end
 
   def render("error.json", %{result: %Changeset{} = changeset}) do
     %{message: translate_errors(changeset)}
+  end
+
+  def render("error.json", %{result: result}) do
+    %{message: result}
   end
 
   defp translate_errors(changeset) do
